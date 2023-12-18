28113cf592ff0f993c40961dac811f1e23a224d8
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-18 11:08:03 -0300

    turn title into a component and add image

--- app/sections/Home.tsx
+++ app/sections/Home.tsx
@@ -1,4 +1,6 @@
 import Header from "../components/Header";
+import IntroTitle from "../components/IntroTitle";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,22 +8,19 @@ export default function Home() {
       <Header />
 
       <main className="flex-1 max-h-[500px] flex items-center justify-between">
-        <div className="leading-tight">
-          <span className="text-spanClamp">hi, I&apos;m</span>
-          <h1
-            className="font-bold text-titleClamp relative cursor-default"
-            data-text='Maik Emanoel'
-            style={{
-              WebkitTextStroke: "1px rgb(241 245 249)",
-              color: 'transparent'
-            }}
-          >
-            Maik Emanoel
-          </h1>
-          <span className="text-spanClamp">a Front-End Developer</span>
-        </div>
+        <IntroTitle />
+        <div className="w-[300px] h-[300px] p-3 rounded-full relative">
+          <Image
+            src="/profile-photo.jpg"
+            width={1536}
+            height={2048}
+            alt="Foto de perfil de Maik Emanoel"
+            priority
+            className="w-full h-full border rounded-full"
+          />
 
-        <div className="w-80 h-80 rounded-full border"></div>
+          <div className="absolute inset-0 border rounded-full border-r-transparent border-l-transparent animate-spin-slow"></div>
+        </div>
       </main>
     </div>
   );
