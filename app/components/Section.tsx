0a7b3430f65306334822bb8a9387a5d8d233811d
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-21 10:58:05 -0300

    add and style title

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,14 +1,16 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 interface SectionProps {
-  id: string,
-  children: ReactNode
+  id: string;
+  title: string;
+  children: ReactNode;
 }
 
-export default function Section({id, children}: SectionProps) {
+export default function Section({ id, title, children }: SectionProps) {
   return (
-    <section id={id} className="h-screen pt-12">
+    <section id={id} className="h-screen pt-10">
+      <h2 className="text-titleClamp tracking-wider text-blue-400">{title}</h2>
       {children}
     </section>
-  )
+  );
 }
