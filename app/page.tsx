98ee3ee75406b675e19f5503cf7cf736b97e8aee
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-23 13:24:35 -0300

    add all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import BgEffect from "./components/BgEffect";
+import ButtonsOnScroll from "./components/ButtonsOnScroll";
+import About from "./sections/About";
 import Home from "./sections/Home";
-import FloatingNavbar from '@/app/components/FloatingNavbar'
+import FloatingNavbar from "@/app/components/FloatingNavbar";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
 
 export default function App() {
   return (
-    <div className="max-w-[1000px] min-h-screen mx-auto px-5 flex flex-col">
+    <div className="max-w-[1000px] min-h-screen mx-auto px-5 flex flex-col relative">
       <Home />
+      <About />
+      <Projects />
+      <Contact />
 
       <FloatingNavbar />
       {/* <BgEffect /> */}
+      <ButtonsOnScroll />
     </div>
-  )
+  );
 }
