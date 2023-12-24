6839cd182251fc8355f5cfafb16aa71e75f27511
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-24 14:19:30 -0300

    refactor and add content

--- app/components/InfiniteSlider.tsx
+++ app/components/InfiniteSlider.tsx
@@ -1,46 +1,93 @@
+"use client";
+
+import { FaHtml5 as htmlLogo, FaReact as reactLogo } from "react-icons/fa";
 import {
-  GithubLogo,
-  TwitchLogo,
-  TwitterLogo,
-  CodaLogo,
-  MetaLogo,
-  LinuxLogo,
-  AppleLogo,
-  FigmaLogo,
-  GoogleLogo,
-  NotionLogo
-} from "@phosphor-icons/react/dist/ssr";
+  IoLogoCss3 as cssLogo,
+  IoLogoGithub as githubLogo,
+} from "react-icons/io";
+import { IoLogoJavascript as javascriptLogo } from "react-icons/io5";
+import {
+  BiLogoTypescript as typescriptLogo,
+  BiLogoVisualStudio as vscodeLogo,
+} from "react-icons/bi";
+import {
+  SiNextdotjs as nextjsLogo,
+  SiTailwindcss as tailwindLogo,
+} from "react-icons/si";
 
 const logos = [
-  <GithubLogo size={32} key={1} />,
-  <TwitchLogo size={32} key={2} />,
-  <TwitterLogo size={32} key={3} />,
-  <CodaLogo size={32} key={4} />,
-  <MetaLogo size={32} key={5} />,
-  <LinuxLogo size={32} key={6} />,
-  <AppleLogo size={32} key={7} />,
-  <FigmaLogo size={32} key={8} />,
-  <GoogleLogo size={32} key={9} />,
-  <NotionLogo size={32} key={10} />, 
+  {
+    image: htmlLogo,
+    name: "HTML",
+  },
+  {
+    image: cssLogo,
+    name: "CSS",
+  },
+  {
+    image: javascriptLogo,
+    name: "Javascript",
+  },
+  {
+    image: typescriptLogo,
+    name: "Typescript",
+  },
+  {
+    image: reactLogo,
+    name: "React",
+  },
+  {
+    image: nextjsLogo,
+    name: "Next.js",
+  },
+  {
+    image: tailwindLogo,
+    name: "Tailwind CSS",
+  },
+  {
+    image: githubLogo,
+    name: "Github",
+  },
+  {
+    image: vscodeLogo,
+    name: "Visual Code",
+  },
 ];
 
-const before = 'before:absolute before:top-0 before:left-0 before:w-[30%] before:h-full before:bg-[linear-gradient(to_left,_transparent,_#0f172a)] before:z-10'
+const before =
+  "before:absolute before:top-0 before:left-0 before:w-[20%] before:h-full before:bg-[linear-gradient(to_left,_transparent,_#0f172a)] before:z-10";
 
-const after = 'after:absolute after:top-0 after:right-0 after:w-[30%] after:h-full after:bg-[linear-gradient(to_right,_transparent,_#0f172a)] after:z-10'
+const after =
+  "after:absolute after:top-0 after:right-0 after:w-[20%] after:h-full after:bg-[linear-gradient(to_right,_transparent,_#0f172a)] after:z-10";
 
 export default function InfiniteSlider() {
   return (
-    <div className={`overflow-hidden py-2 whitespace-nowrap text-slate-300 group relative ${before} ${after}`}>
-      <div className="animate-slide inline-block group-hover:animation-paused">
-        {logos.map((logo, index) => {
-          return <div key={index} className="inline-block mx-10">{logo}</div>;
-        })}
-      </div>
-      <div className="animate-slide inline-block group-hover:animation-paused">
-        {logos.map((logo, index) => {
-          return <div key={index} className="inline-block mx-10">{logo}</div>;
-        })}
-      </div>
+    <div
+      className={`overflow-hidden py-2 whitespace-nowrap text-slate-300 group relative ${before} ${after} slider logos-slide`}
+    >
+      <LogosSlide />
+      <LogosSlide />
     </div>
   );
-};
+}
+
+function LogosSlide() {
+  return (
+    <div className="animate-slide inline-block group-hover:animation-paused">
+      {logos.map((logo) => {
+        return (
+          <div
+            key={logo.name}
+            className="inline-block mx-8 hover:bg-slate-200 hover:text-slate-900 py-2 px-2 rounded-lg cursor-pointer relative group/logo transition-[scale,_opacity] logo"
+          >
+            <logo.image className="w-8 h-8" />
+
+            <div className="hidden absolute left-full top-1/2 -translate-y-1/2 ml-2 py-1 px-3 bg-slate-200 text-slate-900 text-sm font-medium z-50 rounded-md group-hover/logo:block animate-fromLeft">
+              {logo.name}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
