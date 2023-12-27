abf09c68b03a3bb6de7b464712b1317b74bb8fcd
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-27 11:58:21 -0300

    create ProjectsContent

---
+++ app/components/ProjectsContent.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import ProjectCard from "./ProjectCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+export default function ProjectsContent({ reposWithImage }: any) {
+  return (
+    <div className="mt-11">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+          bulletActiveClass: 'bullet-active',
+          bulletClass: 'bullet',
+        }}
+        modules={[Pagination]}
+      >
+        {reposWithImage.map((repoWithImage: any) => {
+          return (
+            <SwiperSlide key={repoWithImage.name}>
+              <ProjectCard repoInfos={repoWithImage} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
