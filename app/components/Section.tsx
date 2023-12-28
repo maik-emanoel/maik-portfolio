014662f486649629b32750311cf17db49b91cdef
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-28 13:27:11 -0300

    change some title styles

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,15 +1,16 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface SectionProps {
+interface SectionProps extends React.HTMLProps<HTMLDivElement> {
   id: string;
   title: string;
   children: ReactNode;
 }
 
-export default function Section({ id, title, children }: SectionProps) {
+export default function Section({ id, title, children, className }: SectionProps) {
   return (
-    <section id={id} className="h-screen pt-10">
-      <h2 className="text-titleClamp tracking-wider text-blue-400">{title}</h2>
+    <section id={id} className={twMerge('h-screen pt-10', className)}>
+      <h2 className="text-titleClamp text-blue-400 font-bold">{title}</h2>
       {children}
     </section>
   );
