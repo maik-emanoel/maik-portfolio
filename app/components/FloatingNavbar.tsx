92bdd8913869375d615c1e94a1a2e9ee137a6383
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-02 11:53:36 -0300

    handle when page reach to the bottom

--- app/components/FloatingNavbar.tsx
+++ app/components/FloatingNavbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { ArrowUp } from "@phosphor-icons/react/dist/ssr";
+import { useEffect, useState } from "react";
 import { MagicTabSelect } from "react-magic-motion";
 
 const pillTabs = [
@@ -24,6 +25,26 @@ const pillTabs = [
 
 export default function FloatingNavbar() {
   const [hoveredIndex, setHoveredIndex] = useState(0);
+  const [isBottom, setIsBottom] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+      const scrollHeight = document.documentElement.scrollHeight;
+
+      console.log(scrollTop, clientHeight, scrollHeight);
+
+      if (scrollTop + clientHeight + 50 >= scrollHeight) {
+        setIsBottom(true);
+      } else {
+        setIsBottom(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.addEventListener("scroll", handleScroll);
+  }, []);
 
   const tabsComponents = pillTabs.map((tab, i) => {
     return (
@@ -42,9 +63,7 @@ export default function FloatingNavbar() {
             id="pillTabs"
             transition={{ type: "spring", bounce: 0.35 }}
           >
-            <span
-              className="rounded-full absolute inset-0 z-10 mix-blend-difference bg-slate-100"
-            />
+            <span className="rounded-full absolute inset-0 z-10 mix-blend-difference bg-slate-100" />
           </MagicTabSelect>
         )}
         {tab.name}
@@ -53,11 +72,19 @@ export default function FloatingNavbar() {
   });
 
   return (
-    <div className="flex items-center gap-3 fixed bottom-8 left-1/2 -translate-x-1/2 border border-zinc-300 px-4 py-2 rounded-full backdrop-blur-md bg-slate-800/25 z-50" 
-    style={{
-      boxShadow: '#00000040 0px 3px 8px'
-    }}>
-      {tabsComponents}
+    <div
+      className="w-fit mx-auto flex items-center gap-3 fixed left-1/2 -translate-x-1/2 bottom-8 border border-zinc-300 px-4 py-2 rounded-full backdrop-blur-md bg-slate-800/25 z-50"
+      style={{
+        boxShadow: "#00000040 0px 3px 8px",
+      }}
+    >
+      {!isBottom ? (
+        tabsComponents
+      ) : (
+        <a href="#home" className="py-2">
+          <ArrowUp size={20} />
+        </a>
+      )}
     </div>
   );
 }
