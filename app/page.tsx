f3251bf1c2596060bc0336c48267d9908521fae3
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-05 13:26:06 -0300

    add Footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,23 @@ import Home from "./sections/Home";
 import FloatingNavbar from "@/app/components/FloatingNavbar";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="max-w-[1000px] min-h-screen mx-auto px-5 flex flex-col relative">
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+    <>
+      <div className="max-w-[1000px] min-h-screen mx-auto px-5 flex flex-col relative">
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
+
+      <Footer />
 
       <FloatingNavbar />
-      {/* <BgEffect /> */}
+      <BgEffect />
       <ButtonsOnScroll />
-    </div>
+    </>
   );
 }
