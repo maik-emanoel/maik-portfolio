461eeb83ecc97138d598d2afe23c1e8ee8a0c89f
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-11 11:41:00 -0300

    style themeButtons

--- app/components/home/ThemeSwitcher.tsx
+++ app/components/home/ThemeSwitcher.tsx
@@ -1,97 +1,142 @@
-  "use client";
+"use client";
 
-  import { Moon, Sun, Desktop } from "@phosphor-icons/react/dist/ssr";
-  import { useEffect, useRef, useState } from "react";
+import { Moon, Sun, Desktop, Check, IconProps } from "@phosphor-icons/react";
+import { useEffect, useRef, useState } from "react";
 
-  type Theme = "light" | "dark";
-  type Options = "light" | "dark" | "system";
+type Theme = "light" | "dark";
+type Options = "light" | "dark" | "system";
 
-  export default function ThemeSwitcher() {
-    const [showOptions, setShowOptions] = useState(false);
-    const [selectedOption, setSelectedOption] = useState<Options | null>(null);
-    const [theme, setTheme] = useState<Theme | null>(null);
+export default function ThemeSwitcher() {
+  const [showOptions, setShowOptions] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<Options | null>(null);
+  const [theme, setTheme] = useState<Theme | null>(null);
+  const [isActive, setIsActive] = useState<Options | null>(null)
 
+  let newTheme = useRef<Theme | null>(null);
+
+  useEffect(() => {
+    const savedThemeOpt = localStorage.getItem("themeOption") as Options;
+    const savedTheme = localStorage.getItem("theme") as Theme;
+
+    setSelectedOption(savedThemeOpt || "dark");
+    setTheme(savedTheme || "dark");
+  }, []);
+
+  useEffect(() => {
     const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
-    let newTheme = useRef<Theme | null>(null)
 
-    useEffect(() => {
-      const savedThemeOpt = localStorage.getItem('themeOption') as Options
-      const savedTheme = localStorage.getItem('theme') as Theme
-
-      setSelectedOption(savedThemeOpt || 'dark')
-      setTheme(savedTheme || 'dark')
-    }, [])
-
-    useEffect(() => {
-      const systemThemeChangeHandler = (event: MediaQueryListEvent) => {
-        if (event.matches) {
-          setTheme("dark");
-          newTheme.current = 'dark'
-          localStorage.setItem('theme', newTheme.current)
-        } else {
-          setTheme("light");
-          newTheme.current = 'light'
-          localStorage.setItem('theme', newTheme.current)
-        }
-      };
-
-      if (selectedOption === "system") {
-        systemTheme.addEventListener("change", systemThemeChangeHandler);
-        return () => {
-          systemTheme.removeEventListener("change", systemThemeChangeHandler);
-        };
-      } else {
-        return systemTheme.removeEventListener("change", systemThemeChangeHandler);
-      }
-    }, [selectedOption, systemTheme]);
-
-    useEffect(() => {
-      const html = document.querySelector("html");
-
-      if (theme === "dark") {
-        html?.classList.add("dark");
-      } else {
-        html?.classList.remove("dark");
-      }
-    }, [selectedOption, theme]);
-
-    function changeOption(selectedOption: Options) {
-      if (selectedOption === "system") {
-        setTheme(systemTheme.matches ? "dark" : "light");
-        newTheme.current = systemTheme.matches ? "dark" : "light"
-      } else if (selectedOption === "light") {
-        setTheme("light");
-        newTheme.current = 'light'
-      } else {
+    const systemThemeChangeHandler = (event: MediaQueryListEvent) => {
+      if (event.matches) {
         setTheme("dark");
-        newTheme.current = 'dark'
+        newTheme.current = "dark";
+        localStorage.setItem("theme", newTheme.current);
+      } else {
+        setTheme("light");
+        newTheme.current = "light";
+        localStorage.setItem("theme", newTheme.current);
       }
+    };
 
-      setSelectedOption(selectedOption);
-      localStorage.setItem('themeOption', selectedOption)
-      localStorage.setItem('theme', newTheme.current)
+    if (selectedOption === "system") {
+      systemTheme.addEventListener("change", systemThemeChangeHandler);
+      return () => {
+        systemTheme.removeEventListener("change", systemThemeChangeHandler);
+      };
+    } else {
+      return systemTheme.removeEventListener(
+        "change",
+        systemThemeChangeHandler
+      );
+    }
+  }, [selectedOption]);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+
+    if (theme === "dark") {
+      html?.classList.add("dark");
+    } else {
+      html?.classList.remove("dark");
+    }
+  }, [selectedOption, theme]);
+
+  function changeOption(selectedOption: Options) {
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (selectedOption === "system") {
+      setTheme(systemTheme.matches ? "dark" : "light");
+      newTheme.current = systemTheme.matches ? "dark" : "light";
+    } else if (selectedOption === "light") {
+      setTheme("light");
+      newTheme.current = "light";
+    } else {
+      setTheme("dark");
+      newTheme.current = "dark";
     }
 
-    return (
-      <>
-        <div
-          className="text-primary rounded-full border border-slate-200 dark:border-muted h-7 w-7 grid place-items-center"
-          onClick={() => setShowOptions((prevState) => !prevState)}
-        >
-          {selectedOption === "light" && <Sun size={20} />}
-          {selectedOption === "dark" && <Moon size={20} />}
-          {selectedOption === "system" && <Desktop size={20} />}
-        </div>
-
-        <div
-          className={`absolute flex gap-2 ${
-            showOptions ? "block" : "hidden"
-          } top-10`}
-        >
-          <span onClick={() => changeOption("light")}>Light</span>
-          <span onClick={() => changeOption("dark")}>Dark</span>
-          <span onClick={() => changeOption("system")}>System preference</span>
-        </div>
-      </>
-    );
+    setSelectedOption(selectedOption);
+    localStorage.setItem("themeOption", selectedOption);
+    localStorage.setItem("theme", newTheme.current);
   }
+
+  return (
+    <>
+      <div
+        className="text-primary rounded-full border border-slate-200 dark:border-muted h-7 w-7 grid place-items-center"
+        onClick={() => setShowOptions((prevState) => !prevState)}
+      >
+        {selectedOption === "light" && <Sun size={20} />}
+        {selectedOption === "dark" && <Moon size={20} />}
+        {selectedOption === "system" && <Desktop size={20} />}
+      </div>
+
+      <div
+        className={`absolute top-12 right-0 z-50 bg-background/20 backdrop-blur-lg shadow-lg overflow-hidden ${
+          showOptions ? "flex" : "hidden"
+        } flex-col border border-slate-200 dark:border-slate-500 rounded-lg`}
+      >
+        <ThemeButton
+          name="Light"
+          icon={Sun}
+          changeOption={() => {changeOption("light"); setIsActive('light')}}
+          isActive={isActive === 'light'}
+        />
+        <ThemeButton
+          name="Dark"
+          icon={Moon}
+          changeOption={() => {changeOption("dark"); setIsActive('dark')}}
+          isActive={isActive === 'dark'}
+        />
+        <ThemeButton
+          name="System"
+          icon={Desktop}
+          changeOption={() => {changeOption("system"); setIsActive('system')}}
+          isActive={isActive === 'system'}
+        />
+      </div>
+    </>
+  );
+}
+
+interface ThemeButtonProps {
+  name: string;
+  icon: React.ElementType<IconProps>;
+  changeOption: () => void;
+  isActive: boolean
+}
+
+function ThemeButton({ name, icon: Icon, changeOption, isActive }: ThemeButtonProps) {
+  return (
+    <div
+      onClick={changeOption}
+      data-isactive={isActive}
+      className="relative flex items-center justify-start gap-2 p-3 pr-10 cursor-pointer overflow-hidden select-none hover:bg-primary/10 data-[isactive=true]:text-blue-secondary"
+    >
+      <Icon size={20} />
+      <span className="text-sm">{name}</span>
+      {isActive && (
+        <Check size={14} className="absolute right-3"/>
+      )}
+    </div>
+  );
+}
