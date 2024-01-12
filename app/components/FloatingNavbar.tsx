9e7d661e34fd7fc54f12ecde8a126863cda3dd72
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-12 13:55:03 -0300

    adjust some styles on sm

--- app/components/FloatingNavbar.tsx
+++ app/components/FloatingNavbar.tsx
@@ -33,7 +33,7 @@ export default function FloatingNavbar() {
       const clientHeight = document.documentElement.clientHeight;
       const scrollHeight = document.documentElement.scrollHeight;
 
-      if (scrollTop + clientHeight + 50 >= scrollHeight) {
+      if (scrollTop + clientHeight + 80 >= scrollHeight) {
         setIsBottom(true);
       } else {
         setIsBottom(false);
@@ -71,7 +71,7 @@ export default function FloatingNavbar() {
   return (
     <div
       data-isbottom={isBottom}
-      className="w-fit mx-auto flex items-center gap-3 fixed left-1/2 -translate-x-1/2 bottom-8 border border-zinc-300 px-4 py-2 rounded-full backdrop-blur-md bg-background dark:bg-slate-800/25 z-50 sm:gap-1 data-[isbottom=true]:p-0 data-[isbottom=true]:hover:bg-primary group data-[isbottom=true]:transition-colors data-[isbottom=true]:duration-200 md:bottom-6 xs:data-[isbottom=true]:bottom-14"
+      className="w-fit mx-auto flex items-center gap-3 fixed left-1/2 -translate-x-1/2 bottom-8 border border-zinc-300 px-4 py-2 rounded-full backdrop-blur-md bg-background dark:bg-slate-800/25 z-50 sm:gap-1 data-[isbottom=true]:p-0 data-[isbottom=true]:hover:bg-primary group data-[isbottom=true]:transition-colors data-[isbottom=true]:duration-200 md:bottom-6 sm:data-[isbottom=true]:bottom-14"
       style={{
         boxShadow: "#00000040 0px 3px 8px",
       }}
