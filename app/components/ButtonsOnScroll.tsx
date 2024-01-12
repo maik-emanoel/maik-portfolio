3e6f4d000d59bf29662547905242ee81f01c5205
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-12 13:55:57 -0300

    receive props to pass to the ThemeSwitcher

--- app/components/ButtonsOnScroll.tsx
+++ app/components/ButtonsOnScroll.tsx
@@ -1,9 +1,28 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import HeaderButtons from "./home/HeaderButtons";
+import Image from "next/image";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import ThemeSwitcher from "./home/ThemeSwitcher";
+import Time from "./home/Time";
+import { Options, Theme } from "./home/HeaderButtons";
 
-export default function ButtonsOnScroll() {
+interface ButtonsOnScrollProps {
+  selectedOption: Options | null;
+  setSelectedOption: Dispatch<SetStateAction<Options | null>>;
+  theme: Theme | null;
+  setTheme: Dispatch<SetStateAction<Theme | null>>;
+  isActive: Options | null;
+  setIsActive: Dispatch<SetStateAction<Options | null>>;
+}
+
+export default function ButtonsOnScroll({
+  selectedOption,
+  setSelectedOption,
+  theme,
+  setTheme,
+  isActive,
+  setIsActive,
+}: ButtonsOnScrollProps) {
   const [scrollValue, setScrollValue] = useState(0);
 
   useEffect(() => {
@@ -15,15 +34,35 @@ export default function ButtonsOnScroll() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  const isScrollDown = scrollValue > 60;
+  const isScrollDown = scrollValue > 70;
 
   return (
-    <>
-      <HeaderButtons
-        className={`fixed ${
-          isScrollDown ? "top-4 translate-y-0" : "top-0 -translate-y-full opacity-30"
-        } right-[24px] border border-slate-200 dark:border-slate-500 py-2 px-3 rounded-full bg-background/5 backdrop-blur-lg z-50 transition-transform duration-300 shadow`}
+    <div
+      className={`flex items-center gap-2 fixed ${
+        isScrollDown
+          ? "top-4 translate-y-0"
+          : "top-0 -translate-y-full opacity-30 overflow-hidden"
+      } right-[24px] border border-slate-200 dark:border-slate-500 py-2 px-3 rounded-full bg-background/80 z-50 transition-transform duration-300 shadow`}
+    >
+      <Time />
+      <span className="h-6 w-[1px] bg-slate-200 dark:bg-slate-500"></span>
+      <div>
+        <Image
+          src="/eua-flag.png"
+          alt="EUA's flag"
+          width={64}
+          height={64}
+          className="h-7 w-7"
+        />
+      </div>
+      <ThemeSwitcher
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+        theme={theme}
+        setTheme={setTheme}
+        isActive={isActive}
+        setIsActive={setIsActive}
       />
-    </>
+    </div>
   );
 }
