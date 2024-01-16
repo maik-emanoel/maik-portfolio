04cc2f004b0fb37a52a06268989b220ff80df4fa
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-16 14:51:09 -0300

    import LangButton and receive labels translations

--- app/components/home/HeaderButtons.tsx
+++ app/components/home/HeaderButtons.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import Image from "next/image";
 import ThemeSwitcher from "./ThemeSwitcher";
 import Time from "./Time";
 import { useState } from "react";
 import ButtonsOnScroll from "../ButtonsOnScroll";
+import LangButton from "./LangButton";
 
 export type Theme = "light" | "dark";
 export type Options = "light" | "dark" | "system";
 
-export default function HeaderButtons() {
+export interface HeaderButtonsProps {
+  lightLabel: string,
+  darkLabel: string,
+  systemLabel: string
+}
+
+export default function HeaderButtons(props: HeaderButtonsProps) {
+  const {lightLabel, darkLabel, systemLabel} = props
+
   const [selectedOption, setSelectedOption] = useState<Options | null>(null);
   const [theme, setTheme] = useState<Theme | null>(null);
   const [isActive, setIsActive] = useState<Options | null>(null);
@@ -19,15 +27,7 @@ export default function HeaderButtons() {
       <div className="flex items-center gap-2 relative">
         <Time />
         <span className="h-6 w-[1px] bg-slate-200 dark:bg-slate-500"></span>
-        <div>
-          <Image
-            src="/eua-flag.png"
-            alt="EUA's flag"
-            width={64}
-            height={64}
-            className="h-7 w-7"
-          />
-        </div>
+        <LangButton />
         <ThemeSwitcher
           selectedOption={selectedOption}
           setSelectedOption={setSelectedOption}
@@ -35,6 +35,9 @@ export default function HeaderButtons() {
           setTheme={setTheme}
           isActive={isActive}
           setIsActive={setIsActive}
+          lightLabel={lightLabel}
+          darkLabel={darkLabel}
+          systemLabel={systemLabel}
         />
       </div>
 
@@ -45,6 +48,9 @@ export default function HeaderButtons() {
         setTheme={setTheme}
         isActive={isActive}
         setIsActive={setIsActive}
+        lightLabel={lightLabel}
+        darkLabel={darkLabel}
+        systemLabel={systemLabel}
       />
     </>
   );
