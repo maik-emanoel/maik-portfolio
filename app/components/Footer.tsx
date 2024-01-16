25fc0ad3781a4a99865977212a68ef220b10c8a2
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-16 14:42:39 -0300

    use useTranslations

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,11 +5,14 @@ import {
   LinkedinLogo,
 } from "@phosphor-icons/react/dist/ssr";
 import { IconProps } from "@phosphor-icons/react";
+import { useTranslations } from "next-intl";
 
 export default function Footer() {
+  const t = useTranslations("footer")
+
   return (
     <footer className="relative max-w-[1000px] bottom-8 mx-auto px-5 flex justify-between items-center text-muted xs:bottom-4">
-      <p className="text-xs">Created by Maik Emanoel</p>
+      <p className="text-xs">{t("created-by")} Maik Emanoel</p>
 
       <div className="flex gap-4 xs:gap-2">
         <SocialLink
