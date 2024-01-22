302f457949474ae85feea2ceb1287236f2b0bd55
Maik Emanoel <maikemanoel11@gmail.com>
2024-01-22 10:46:38 -0300

    set localeDetection as false

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['en', 'pt'],
  
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localeDetection: false
 });
  
 export const config = {
