f235d2ba71ab9b074974e50dfd64053d6b8c34ed
Maik Emanoel <maikemanoel11@gmail.com>
2024-02-27 14:44:15 -0300

    refactor: move logos array to a file and import it

--- app/components/about/InfiniteSlider.tsx
+++ app/components/about/InfiniteSlider.tsx
@@ -1,67 +1,7 @@
 "use client";
 
-import {
-  FaHtml5 as htmlLogo,
-  FaReact as reactLogo,
-  FaGitAlt as gitLogo,
-} from "react-icons/fa";
-import {
-  IoLogoCss3 as cssLogo,
-  IoLogoGithub as githubLogo,
-} from "react-icons/io";
-import { IoLogoJavascript as javascriptLogo } from "react-icons/io5";
-import {
-  BiLogoTypescript as typescriptLogo,
-  BiLogoVisualStudio as vscodeLogo,
-} from "react-icons/bi";
-import {
-  SiNextdotjs as nextjsLogo,
-  SiTailwindcss as tailwindLogo,
-} from "react-icons/si";
 import { useIsTouchSupported } from "@/app/utils/touchUtils";
-
-const logos = [
-  {
-    image: htmlLogo,
-    name: "HTML",
-  },
-  {
-    image: cssLogo,
-    name: "CSS",
-  },
-  {
-    image: javascriptLogo,
-    name: "Javascript",
-  },
-  {
-    image: typescriptLogo,
-    name: "Typescript",
-  },
-  {
-    image: reactLogo,
-    name: "React",
-  },
-  {
-    image: nextjsLogo,
-    name: "Next.js",
-  },
-  {
-    image: tailwindLogo,
-    name: "Tailwind CSS",
-  },
-  {
-    image: githubLogo,
-    name: "Github",
-  },
-  {
-    image: gitLogo,
-    name: "Git",
-  },
-  {
-    image: vscodeLogo,
-    name: "Visual Code",
-  },
-];
+import { logos } from "@/app/lib/about-logos";
 
 const before =
   "before:absolute before:top-0 before:left-0 before:w-[15%] before:h-full before:bg-[linear-gradient(to_left,_transparent,_#fff)] before:dark:bg-[linear-gradient(to_left,_transparent,_#0f172a)] before:z-10";
