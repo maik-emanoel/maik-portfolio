ee38f75930922de57e616ea11966730f8922bebb
Maik Emanoel <maikemanoel11@gmail.com>
2024-02-27 14:43:13 -0300

    refactor: export const logos

---
+++ app/lib/about-logos.ts
@@ -0,0 +1,67 @@
+import {
+  FaHtml5 as htmlLogo,
+  FaReact as reactLogo,
+  FaGitAlt as gitLogo,
+} from "react-icons/fa";
+import {
+  IoLogoCss3 as cssLogo,
+  IoLogoGithub as githubLogo,
+} from "react-icons/io";
+import { IoLogoJavascript as javascriptLogo } from "react-icons/io5";
+import {
+  BiLogoTypescript as typescriptLogo,
+  BiLogoVisualStudio as vscodeLogo,
+  BiLogoPostgresql as postgresLogo
+} from "react-icons/bi";
+import {
+  SiNextdotjs as nextjsLogo,
+  SiTailwindcss as tailwindLogo,
+  SiPrisma as prismaLogo
+} from "react-icons/si";
+
+export const logos = [
+  {
+    image: htmlLogo,
+    name: "HTML",
+  },
+  {
+    image: cssLogo,
+    name: "CSS",
+  },
+  {
+    image: javascriptLogo,
+    name: "Javascript",
+  },
+  {
+    image: typescriptLogo,
+    name: "Typescript",
+  },
+  {
+    image: reactLogo,
+    name: "React",
+  },
+  {
+    image: nextjsLogo,
+    name: "Next.js",
+  },
+  {
+    image: tailwindLogo,
+    name: "Tailwind CSS",
+  },
+  {
+    image: prismaLogo,
+    name: "Prisma"
+  },
+  {
+    image: postgresLogo,
+    name: "PostgreSQL"
+  },
+  {
+    image: githubLogo,
+    name: "Github",
+  },
+  {
+    image: gitLogo,
+    name: "Git",
+  }
+];
