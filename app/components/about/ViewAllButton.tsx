358aa2333afc97f9a4cd145ee0119953a9a10344
Maik Emanoel <maikemanoel11@gmail.com>
2024-02-28 11:01:42 -0300

    refactor: pass viewAllButtonTitle and solve some problems

--- app/components/about/ViewAllButton.tsx
+++ app/components/about/ViewAllButton.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { logos } from "@/app/lib/about-logos";
-import { X } from "@phosphor-icons/react";
+import { DiamondsFour, X } from "@phosphor-icons/react";
 import { useEffect, useRef, useState, MouseEvent } from "react";
 
-export default function ViewAllButton() {
+export default function ViewAllButton({
+  viewAllButtonTitle,
+}: {
+  viewAllButtonTitle: string;
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const portalRef = useRef(null);
 
@@ -16,7 +20,8 @@ export default function ViewAllButton() {
       document.body.style.overflowY = "initial";
     }
 
-    return () => window.removeEventListener("keydown", handleCloseModalWithHotkey);
+    return () =>
+      window.removeEventListener("keydown", handleCloseModalWithHotkey);
   }, [isModalOpen]);
 
   function handleShowModal() {
@@ -30,20 +35,21 @@ export default function ViewAllButton() {
   }
 
   function handleCloseModalWithHotkey(e: KeyboardEvent) {
-    console.log('oi')
-    if (e.key === 'Escape') {
-      setIsModalOpen(false)
+    if (e.key === "Escape") {
+      setIsModalOpen(false);
     }
   }
 
   return (
     <>
-      <span
-        className="self-end text-sm text-blue-primary cursor-pointer hover:text-blue-secondary"
+      <button
+        type="button"
+        className="text-sm text-blue-primary cursor-pointer hover:text-blue-secondary scroll-reveal"
         onClick={handleShowModal}
+        title={viewAllButtonTitle}
       >
-        View all
-      </span>
+        <DiamondsFour size={20} weight="fill" className="sm:size-4" />
+      </button>
 
       {isModalOpen && (
         <div
@@ -51,8 +57,8 @@ export default function ViewAllButton() {
           ref={portalRef}
           onClick={handleCloseModal}
         >
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl w-[75%] px-8 py-7 pr-6 rounded-lg h-[70vh] bg-background flex flex-col gap-8 shadow-xl">
-            <h3 className="text-3xl">All my stacks</h3>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl w-[75%] px-8 py-7 pr-6 rounded-lg h-[70vh] bg-background flex flex-col gap-8 shadow-xl border border-white/10 sm:w-screen sm:h-dvh sm:rounded-none">
+            <h3 className="text-3xl font-medium">All my stacks</h3>
             <X
               size={24}
               className="absolute top-4 right-4 rounded-full bg-white/10 p-1 cursor-pointer"
