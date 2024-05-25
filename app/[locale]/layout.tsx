b48885d09e8480af7eef42e0ac4d8f0f35fc055a
Maik Emanoel <maikemanoel11@gmail.com>
2024-05-25 17:07:46 -0300

    refactor: move metadata obj to a file and wrap the children with synchronizeProvider

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,34 +3,12 @@ import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import "@/app/scroll.css";
 import { Providers } from "../providers";
-import favicon from "@/public/favicon.svg";
-import openGraphImage from "@/app/[locale]/opengraph-image.svg"
+import { SynchronizeProvider } from "../contexts/synchronizeContext";
+import { metadataOptions } from "../utils/metadata-options";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://maik-emanoel.vercel.app'),
-  title: "Maik Emanoel | Portfolio",
-  description:
-    "Welcome to Maik Emanoel's portfolio. Dive into a world of web development, where each project is a unique journey. Discover the intersection of creativity and technology. Let's connect and explore the possibilities together. Your digital journey starts here!",
-  creator: "Maik Emanoel",
-  keywords: [
-    "Web Development",
-    "Frontend Development",
-    "React",
-    "Next.js",
-    "JavaScript",
-    "Typescript",
-    "Portfolio",
-    "Tailwind",
-  ],
-  icons: [{ rel: "icon", url: favicon.src }],
-  openGraph: {
-    title: 'Maik Emanoel | Portfolio',
-    description: "Welcome to Maik Emanoel's portfolio",
-    images: openGraphImage.src,
-  },
-};
+export const metadata: Metadata = metadataOptions
 
 export default function RootLayout({
   children,
@@ -49,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} min-h-screen bg-background text-primary overflow-x-hidden`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <SynchronizeProvider>{children}</SynchronizeProvider>
+        </Providers>
       </body>
     </html>
   );
