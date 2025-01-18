c635c7435211587fc9118bacd64917df6223d2a2
Maik Emanoel <maikemanoel11@gmail.com>
2025-01-18 00:18:03 -0300

    refactor: streamline Projects component and improve error handling

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -1,61 +1,39 @@
-import { getTranslations } from "next-intl/server";
-import ProjectsContent from "../components/projects/ProjectsContent";
-import Section from "../components/Section";
-import { fetchGithubRepos, fetchReadmeImage } from "../services/api/githubApi";
+import { getTranslations } from 'next-intl/server'
+import ProjectsContent from '../components/projects/ProjectsContent'
+import Section from '../components/Section'
+import { fetchGithubRepos, fetchReadmeImage } from '../services/api/githubApi'
 
 export default async function Projects() {
-  const t = await getTranslations("projects");
+  const t = await getTranslations('projects')
 
   const projectCardLabels = {
-    titleTopic: t("title-topic"),
-    firstLink: t("first-link"),
-    secondLink: t("second-link"),
-  };
-
-  try {
-    const data = await fetchGithubRepos();
-
-    const reposFiltered = data.filter((repo: any) =>
-      repo.topics.includes("mk")
-    );
-
-    const reposWithImage = await Promise.all(
-      reposFiltered.map(async (repoFiltered: any) => {
-        try {
-          return await fetchReadmeImage(repoFiltered);
-        } catch (error) {
-          console.log("Failed to fetch data", error);
-        }
-      })
-    );
-
-    return (
-      <Section
-        key={"projects"}
-        title={t("section-name")}
-        id="projects"
-        className="sm:h-svh"
-      >
-        <ProjectsContent
-          reposWithImage={reposWithImage}
-          t={projectCardLabels}
-        />
-      </Section>
-    );
-  } catch (error) {
-    console.log("Failed to fetch repositories", error);
-
-    return (
-      <Section
-        key={"projects"}
-        title={t("section-name")}
-        id="projects"
-        className="sm:h-svh"
-      >
-        <div className="h-full flex flex-col mt-[25%]">
-          <p className="self-center text-2xl">Sorry, something went wrong...</p>
-        </div>
-      </Section>
-    );
+    titleTopic: t('title-topic'),
+    firstLink: t('first-link'),
+    secondLink: t('second-link'),
   }
+
+  const data = await fetchGithubRepos()
+
+  const reposFiltered = data.filter((repo: any) => repo.topics.includes('mk'))
+
+  const reposWithImage = await Promise.all(
+    reposFiltered.map(async (repoFiltered: any) => {
+      try {
+        return await fetchReadmeImage(repoFiltered)
+      } catch (error) {
+        console.log('Failed to fetch data', error)
+      }
+    })
+  )
+
+  return (
+    <Section
+      key={'projects'}
+      title={t('section-name')}
+      id="projects"
+      className="sm:h-svh"
+    >
+      <ProjectsContent reposWithImage={reposWithImage} t={projectCardLabels} />
+    </Section>
+  )
 }
