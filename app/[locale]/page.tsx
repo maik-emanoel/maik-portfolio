91c60228702f012fcad9458f107ba9e40c3c9b97
Maik Emanoel <maikemanoel11@gmail.com>
2025-01-18 00:36:06 -0300

    refactor: remove Suspense around Projects section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,52 +1,47 @@
-import { useTranslations } from "next-intl";
-import { Suspense } from "react";
+import { useTranslations } from 'next-intl'
 
-import BgEffect from "../components/BgEffect";
-import About from "../sections/About";
-import Home from "../sections/Home";
-import FloatingNavbar from "@/app/components/FloatingNavbar";
-import Projects from "../sections/Projects";
-import Contact from "../sections/Contact";
-import Footer from "../components/Footer";
-import ProjectsLoading from "../components/projects/ProjectsLoading";
+import BgEffect from '../components/BgEffect'
+import About from '../sections/About'
+import Home from '../sections/Home'
+import FloatingNavbar from '@/app/components/FloatingNavbar'
+import Projects from '../sections/Projects'
+import Contact from '../sections/Contact'
+import Footer from '../components/Footer'
 
 export default function App() {
-  const t = useTranslations("floating-navbar");
+  const t = useTranslations('floating-navbar')
 
   const pillTabs = [
     {
-      name: t("home"),
-      path: "#home",
+      name: t('home'),
+      path: '#home',
     },
     {
-      name: t("about"),
-      path: "#about",
+      name: t('about'),
+      path: '#about',
     },
     {
-      name: t("projects"),
-      path: "#projects",
+      name: t('projects'),
+      path: '#projects',
     },
     {
-      name: t("contact"),
-      path: "#contact",
+      name: t('contact'),
+      path: '#contact',
     },
-  ];
+  ]
 
   return (
     <>
       <div className="max-w-[1000px] min-h-screen mx-auto px-5 flex flex-col relative">
         <Home />
         <About />
-        <Suspense fallback={<ProjectsLoading />}>
-          <Projects />
-        </Suspense>
+        <Projects />
         <Contact />
         <Footer />
       </div>
 
-
       <FloatingNavbar pillTabs={pillTabs} />
       <BgEffect />
     </>
-  );
+  )
 }
